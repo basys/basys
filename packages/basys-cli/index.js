e81ae2be52ca75a1e0c6d6e2f724aadb5fd50cd8
Sergey <sergkop@gmail.com>
2018-01-27 01:05:39 +0300

    CLI refactoring

--- packages/basys-cli/index.js
+++ packages/basys-cli/index.js
@@ -1,25 +1,7 @@
 #!/usr/bin/env node
 
-const {exec} = require('child_process');
-const fs = require('fs-extra');
-const inquirer = require('inquirer');
-const ora = require('ora');
-const path = require('path');
-const util = require('util');
 const yargs = require('yargs');
-
-// Detect whether `dir` is inside a Basys project directory
-function detectBasysProject(dir) {
-  while (true) {
-    if (dir === path.dirname(dir)) break;
-
-    if (fs.existsSync(path.join(dir, 'basys.json')) && fs.existsSync(path.join(dir, 'package.json'))) {
-      return dir;
-    }
-
-    dir = path.dirname(dir);
-  }
-}
+const {detectBasysProject, initProject} = require('./utils');
 
 const projectDir = detectBasysProject(process.cwd());
 let cli = yargs.usage('$0 <command> [<arguments>]');
@@ -76,93 +58,7 @@ async function runCommand() {
       // BUG: show help for command if provided
       yargs.showHelp();
     } else if (command === 'init') {
-      // Starter project can be an absolute/relative path or a github repo (with an optional branch)
-      let templateName = argv['template-name'];
-      let destDir;
-      let promise = Promise.resolve();
-      if (!argv['template-name']) {
-        // If starter project is not provided in command arguments ask to select one
-        promise = inquirer
-          .prompt([
-            {
-              type: 'list',
-              name: 'name',
-              message: 'Select a starter project',
-              choices: [
-                {name: 'Blank project', value: 'basys/basys-starter-project'},
-                {name: 'Todo list sample web app', value: 'basys/basys-todomvc'},
-              ],
-            },
-          ])
-          .then(answers => {
-            templateName = answers.name;
-          });
-      }
-
-      let spinner;
-      return promise
-        .then(() =>
-          inquirer.prompt([
-            {
-              type: 'input',
-              name: 'dest',
-              message: 'Project location',
-              default: '.',
-              validate(dest) {
-                if (!dest || dest.startsWith('.')) {
-                  destDir = path.join(process.cwd(), dest);
-                } else {
-                  destDir = dest;
-                }
-                destDir = path.normalize(destDir);
-
-                if (detectBasysProject(destDir)) return false; // BUG: provide error message about another Basys project
-                // BUG: check that directory doesn't exist or empty, attempt to create if needed or show an error.
-
-                return true;
-              },
-            },
-          ]),
-        )
-        .then(() => {
-          spinner = ora('Downloading starter project').start();
-          if (path.isAbsolute(templateName) || templateName.startsWith('.') || templateName.startsWith('~')) {
-            // Local directory
-            let templateDir;
-            if (templateName.startsWith('.')) {
-              templateDir = path.join(process.cwd(), templateName);
-            } else {
-              templateDir = path.normalize(templateName);
-            }
-            return fs.copy(templateDir, destDir);
-          } else {
-            // Github repository
-            const m = /^([^/]+)\/([^#]+)(#(.+))?$/.exec(templateName);
-            const url = `https://github.com/${m[1]}/${m[2]}/archive/${m[4] || 'master'}.zip`;
-            const downloadUrl = require('download');
-            return downloadUrl(url, destDir, {
-              extract: true,
-              strip: 1,
-              mode: '666',
-              headers: {accept: 'application/zip'},
-            });
-          }
-        })
-        .then(async () => {
-          if (!detectBasysProject(destDir)) {
-            spinner.stop();
-            throw new Error('Project provided with starter template is missing basys.json or package.json file');
-          }
-
-          spinner.text = 'Installing packages';
-          process.chdir(destDir);
-          await util.promisify(exec)('npm install');
-
-          spinner.stop();
-          // BUG: add a message about 'cd' (what about windows?), only if needed
-          // BUG: add colors? change message if this is a local basys-cli instance?
-          spinner.succeed('Successfully generated the project. To start the dev server run `basys dev`.');
-        });
+      initProject({name: argv['template-name']});
     }
   }
 }
@@ -172,3 +68,5 @@ runCommand().catch(err => {
   console.log(err.stack);
   process.exit(1);
 });
+
+process.on('SIGINT', () => process.exit());
---
+++ packages/basys-cli/utils.js
@@ -0,0 +1,125 @@
+const {exec} = require('child_process');
+const fs = require('fs-extra');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const path = require('path');
+const util = require('util');
+
+// Detect whether `dir` is inside a Basys project directory
+function detectBasysProject(dir) {
+  while (true) {
+    if (dir === path.dirname(dir)) break;
+
+    if (fs.existsSync(path.join(dir, 'basys.json')) && fs.existsSync(path.join(dir, 'package.json'))) {
+      return dir;
+    }
+
+    dir = path.dirname(dir);
+  }
+}
+
+// `answers.name` can be an absolute/relative directory path or a github repo (with an optional branch)
+function initProject(answers, install = true) {
+  let promise;
+  let templateName;
+  if (answers.name) {
+    templateName = answers.name;
+    promise = Promise.resolve();
+  } else {
+    // If starter project is not provided in command arguments ask to select one
+    promise = inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'name',
+          message: 'Select a starter project',
+          choices: [
+            {name: 'Blank project', value: 'basys/basys-starter-project'},
+            {name: 'Todo list sample web app', value: 'basys/basys-todomvc'},
+          ],
+        },
+      ])
+      .then(answers => {
+        templateName = answers.name;
+      });
+  }
+
+  let destDir;
+  let spinner;
+
+  function validateDestPath(dest) {
+    if (!dest || dest.startsWith('.')) {
+      destDir = path.join(process.cwd(), dest);
+    } else {
+      destDir = dest;
+    }
+    destDir = path.normalize(destDir);
+
+    if (detectBasysProject(destDir)) return 'Provided location is already inside another Basys project';
+    // BUG: check that directory doesn't exist or empty, attempt to create if needed or show an error
+
+    return true;
+  }
+
+  return promise
+    .then(() => {
+      if (answers.dest) {
+        const res = validateDestPath(answers.dest);
+        if (typeof res === 'string') throw Error(res);
+        return Promise.resolve();
+      } else {
+        return inquirer.prompt([
+          {
+            type: 'input',
+            name: 'dest',
+            message: 'Project location',
+            default: '.',
+            validate: validateDestPath,
+          },
+        ]);
+      }
+    })
+    .then(() => {
+      spinner = ora('Downloading starter project').start();
+      if (path.isAbsolute(templateName) || templateName.startsWith('.') || templateName.startsWith('~')) {
+        // Local directory
+        let templateDir;
+        if (templateName.startsWith('.')) {
+          templateDir = path.join(process.cwd(), templateName);
+        } else {
+          templateDir = path.normalize(templateName);
+        }
+        return fs.copy(templateDir, destDir);
+      } else {
+        // Github repository
+        const m = /^([^/]+)\/([^#]+)(#(.+))?$/.exec(templateName);
+        const url = `https://github.com/${m[1]}/${m[2]}/archive/${m[4] || 'master'}.zip`;
+        const downloadUrl = require('download');
+        return downloadUrl(url, destDir, {
+          extract: true,
+          strip: 1,
+          mode: '666',
+          headers: {accept: 'application/zip'},
+        });
+      }
+    })
+    .then(async () => {
+      if (!detectBasysProject(destDir)) {
+        spinner.stop();
+        throw new Error('Project provided with starter template is missing basys.json or package.json file');
+      }
+
+      if (install) {
+        spinner.text = 'Installing packages';
+        process.chdir(destDir);
+        await util.promisify(exec)('npm install');
+      }
+
+      spinner.stop();
+      // BUG: add a message about 'cd' (what about windows?), only if needed
+      // BUG: add colors? change message if this is a local basys-cli instance?
+      spinner.succeed('Successfully generated the project. To start the dev server run `basys dev`.');
+    });
+}
+
+module.exports = {detectBasysProject, initProject};
