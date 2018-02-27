b4b9085c91ce1bfd57eb2b571515ae43709989bc
Sergey <sergkop@gmail.com>
2018-02-28 00:12:20 +0300

    ESLint config improvements

--- packages/eslint-config-basys/rules/imports.js
+++ packages/eslint-config-basys/rules/imports.js
@@ -8,13 +8,9 @@ module.exports = {
     'import/ignore': ['node_modules', '\\.(scss|css|less|svg|json)$'],
   },
   rules: {
-    // Static analysis:
-
     // ensure imports point to files/modules that can be resolved
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
     'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
-
-    // Helpful warnings:
 
     // disallow invalid exports, e.g. multiple defaults
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/export.md
@@ -28,43 +24,26 @@ module.exports = {
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default-member.md
     'import/no-named-as-default-member': 'error',
 
-    // BUG: fix it
     // Forbid the use of extraneous packages
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
-    // paths are treated both as absolute paths, and relative to process.cwd()
     'import/no-extraneous-dependencies': [
       'error',
       {
-        devDependencies: [
-          'test/**', // common npm pattern
-          'tests/**', // also common npm pattern
-          'spec/**', // mocha, rspec-like pattern
-          '**/__tests__/**', // jest pattern
-          'test.js', // repos with a single test file
-          'test-*.js', // repos with multiple top-level test files
-          '**/*.{test,spec}.js', // tests where the extension denotes that it is a test
-          '**/jest.config.js', // jest config
-        ],
-        optionalDependencies: false,
+        devDependencies: ['tests/**'],
       },
     ],
-    // 'import/no-extraneous-dependencies': 'off', // BUG: shouldn't report built-in dependencies
 
     // Forbid mutable exports
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-mutable-exports.md
     'import/no-mutable-exports': 'error',
 
-    // Module systems:
-
     // disallow AMD require/define
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-amd.md
     'import/no-amd': 'error',
 
-    // Style guide:
-
     // disallow non-import statements appearing before import statements
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
-    'import/first': ['error', 'absolute-first'],
+    'import/first': 'error',
 
     // disallow duplicate imports
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
@@ -77,10 +56,6 @@ module.exports = {
     // Forbid import of modules using absolute paths
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-absolute-path.md
     'import/no-absolute-path': 'error',
-
-    // Forbid require() calls with expressions
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
-    // 'import/no-dynamic-require': 'error',
 
     // Forbid Webpack loader syntax in imports
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-webpack-loader-syntax.md
